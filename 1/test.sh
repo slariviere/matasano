74c818fc98777812017d8f80f8710989d013a903
Sébastien Larivière <sebastien@lariviere.me>
2014-12-31 00:52:05 -0500

    Challenge 1 complete with python library

--- 1/test.sh
+++ 1/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ $(python base64.py 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d) == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" ]]; then
-    echo "Pass"
+if [[ $(python base64_lib.py 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d) == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" ]]; then
+    echo -e "\nPass"
     exit 0
 else
-    echo "Fail"
+    echo -e "\nFail"
     exit 1
 fi
