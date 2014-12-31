13bef46219622bd355588d4c6088419e507f2e4b
Sébastien Larivière <sebastien@lariviere.me>
2014-12-30 21:58:04 -0500

    Adding exit value to test

--- 1/test.sh
+++ 1/test.sh
@@ -2,6 +2,8 @@
 
 if [ $(python base64.py 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d) = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" ]; then
     echo "Pass"
+    exit 0
 else
     echo "Fail"
+    exit 1
 fi
